870eebd22c4a13465cea3c1c18eab98177e9e7c8
AndrewTaker <n0thingg@yandex.ru>
2026-03-28 20:43:15 +0300

    add movies dto

--- internal/api/dto.go
+++ internal/api/dto.go
@@ -4,3 +4,11 @@ type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type MoviesResponse struct {
+	ID     string   `json:"id"`
+	Title  string   `json:"title"`
+	Genres []string `json:"genres"`
+	Year   int      `json:"year"`
+	Rating float64  `json:"rating"`
+}
