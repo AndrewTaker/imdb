f27788a2b174e1b6beac168605c983e1ae93500d
AndrewTaker <n0thingg@yandex.ru>
2026-03-28 15:10:22 +0300

    add a bit of seeding on mongo startup

---
+++ internal/database/seed.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func createCollection(d *mongo.Database, name string) error {
+	return d.CreateCollection(context.Background(), name)
+}
+
+func seedCollections(d *mongo.Database, names []string) error {
+	for _, name := range names {
+		if err := createCollection(d, name); err != nil {
+			return fmt.Errorf("failed to create collection %s", name)
+		}
+	}
+
+	return nil
+}
+
+func ensureIndexes(d *mongo.Database) error {
+	userIndex := mongo.IndexModel{
+		Keys: bson.D{
+			{Key: "email", Value: 1},
+		},
+		Options: options.Index().SetUnique(true),
+	}
+
+	_, err := d.Collection("users").Indexes().CreateOne(context.Background(), userIndex)
+	return err
+}
